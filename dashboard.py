ca256ab7a18f7840e72596065aff402554e9ac15
Frank <frank@appbackr.com>
2014-12-08 15:39:58 +0800

    Initial commit of flask wrapper to serve dashboard

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+#!flask/bin/python
+from flask import Flask, request
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/<path:filename>')
+def static_proxy(filename):
+	return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+	app.run(debug=True)
